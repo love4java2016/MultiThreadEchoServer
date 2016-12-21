da76283e9ba56d877cb72159039738f322edc310
guopeng <guopeng600@126.com>
2016-12-21 14:38:24 +0800

    init - java project

---
+++ MultiThreadEchoServer/src/com/MultiThreadEchoServer.java
@@ -0,0 +1,72 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultiThreadEchoServer {
+    private static ExecutorService tp = Executors.newCachedThreadPool();
+
+    static class HandleMsg implements Runnable {
+        Socket clientSocket = null;
+
+        public HandleMsg(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            PrintWriter os = null;
+            BufferedReader is = null;
+            try {
+                is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                os = new PrintWriter(clientSocket.getOutputStream(), true);
+                String inputLine = null;
+                long b = System.currentTimeMillis();
+                while ((inputLine = is.readLine()) != null) {
+                    os.println(inputLine + "=======");
+                }
+                long e = System.currentTimeMillis();
+                System.out.println("spend:" + (e - b) + "ms");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                if(is != null)
+                    try {
+                        is.close();
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                if(os != null)os.close();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        ServerSocket echoServer = null;
+        Socket clientSocket = null;
+        try {
+            echoServer = new ServerSocket(8000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                clientSocket = echoServer.accept();
+                System.out.println(clientSocket.getRemoteSocketAddress() + "connect!");
+                tp.execute(new HandleMsg(clientSocket));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
